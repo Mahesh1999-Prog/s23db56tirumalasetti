71a685c939e8446b5e94c46fee0710814349bfe8
Mahesh1999-Prog <143574719+Mahesh1999-Prog@users.noreply.github.com>
2023-11-09 20:04:29 -0600

    GET request for one fruit

--- controllers/fruit.js
+++ controllers/fruit.js
@@ -12,9 +12,17 @@ exports.fruit_list = async function(req, res) {
    };
    
 // for a specific fruit.
-exports.fruit_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: fruit detail: ' + req.params.id);
-};
+// for a specific fruit.
+exports.fruit_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await fruit.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
  
 // Handle fruit create on POST.
 exports.fruit_create_post = async function(req, res) {
