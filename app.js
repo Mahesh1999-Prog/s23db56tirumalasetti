e7c5cf1b8d12c72c38ecce1e83e745225355e2e7
Mahesh1999-Prog <143574719+Mahesh1999-Prog@users.noreply.github.com>
2023-11-17 11:12:49 -0600

    commiting the account and other operations

--- app.js
+++ app.js
@@ -3,6 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
 
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
@@ -13,8 +34,9 @@ mongoose.connect(connectionString);
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -24,32 +46,40 @@ var chooseRouter = require('./routes/choose');
 var fruit = require("./models/fruit");
 var resourceRouter = require('./routes/resource')
 
-async function recreateDB(){
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+async function recreateDB() {
   // Delete everything
   await fruit.deleteMany();
-  let instance1 = new fruit({name: "Apple", color: "Red", quantity: 12});
-  let instance2 = new fruit({name: "Banana", color: "Yellow", quantity: 24});
-  let instance3 = new fruit({name: "Orange", color: "Orange", quantity: 6});
+  let instance1 = new fruit({ name: "Apple", color: "Red", quantity: 12 });
+  let instance2 = new fruit({ name: "Banana", color: "Yellow", quantity: 24 });
+  let instance3 = new fruit({ name: "Orange", color: "Orange", quantity: 6 });
 
-  instance1.save().then(doc=>{
-  console.log("First object saved")}
-  ).catch(err=>{
-  console.error(err)
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }
+  ).catch(err => {
+    console.error(err)
   });
-  instance2.save().then(doc=>{
-  console.log("Second object saved")}
-  ).catch(err=>{
-  console.error(err)
+  instance2.save().then(doc => {
+    console.log("Second object saved")
+  }
+  ).catch(err => {
+    console.error(err)
   });
- instance3.save().then(doc=>{
-  console.log("Third object saved")}
-  ).catch(err=>{
-  console.error(err)
+  instance3.save().then(doc => {
+    console.log("Third object saved")
+  }
+  ).catch(err => {
+    console.error(err)
   });
- }
- let reseed = true;
- if (reseed) {recreateDB();}
- 
+}
+let reseed = true;
+if (reseed) { recreateDB(); }
+
 
 var app = express();
 
@@ -61,6 +91,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -70,13 +107,14 @@ app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
 
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
