9ed0f1c2a229f1517967b644ec9d1a0c165dae4c
Mahesh1999-Prog <143574719+Mahesh1999-Prog@users.noreply.github.com>
2023-11-29 18:42:27 -0600

    updated file

--- routes/fruit.js
+++ routes/fruit.js
@@ -1,6 +1,15 @@
 var express = require('express');
 const fruit_controlers= require('../controllers/fruit');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on 
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ res.redirect("/login");
+ }
 /* GET fruits */
 router.get('/', fruit_controlers.fruit_view_all_Page );
 /* GET detail fruit page */
@@ -8,7 +17,7 @@ router.get('/detail', fruit_controlers.fruit_view_one_Page);
 /* GET create fruit page */
 router.get('/create', fruit_controlers.fruit_create_Page);
 /* GET create update page */
-router.get('/update', fruit_controlers.fruit_update_Page);
+router.get('/update', secured,fruit_controlers.fruit_update_Page);
 /* GET delete fruit page */
 router.get('/delete', fruit_controlers.fruit_delete_Page);
 module.exports = router;
