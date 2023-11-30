547410a208729962921a2a8fe5ff0e369427b312
Mahesh1999-Prog <143574719+Mahesh1999-Prog@users.noreply.github.com>
2023-11-29 19:29:18 -0600

    bonus update

--- models/fruit.js
+++ models/fruit.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose")
 const fruitSchema = mongoose.Schema({
-name: String,
-color: String,
+name: { type : String,
+    minlength : 4,
+    maxlength : 12
+},
+color: { type : String,
+    minlength : 3,
+    maxlength : 6
+},
 quantity: { type : Number,
     min : 2,
     max : 200000
--- routes/fruit.js
+++ routes/fruit.js
@@ -15,9 +15,9 @@ router.get('/', fruit_controlers.fruit_view_all_Page );
 /* GET detail fruit page */
 router.get('/detail', fruit_controlers.fruit_view_one_Page);
 /* GET create fruit page */
-router.get('/create', fruit_controlers.fruit_create_Page);
+router.get('/create', secured, fruit_controlers.fruit_create_Page);
 /* GET create update page */
 router.get('/update', secured,fruit_controlers.fruit_update_Page);
 /* GET delete fruit page */
-router.get('/delete', fruit_controlers.fruit_delete_Page);
+router.get('/delete', secured, fruit_controlers.fruit_delete_Page);
 module.exports = router;
